077682c2e1df50464ef856294de1bc9ba3eefcd6
Kevin Darbyshire-Bryant <kevin@darbyshire-bryant.me.uk>
2015-11-14 10:10:04 +0000

    Describe nosquash option

--- tc/q_cake.c
+++ tc/q_cake.c
@@ -55,7 +55,7 @@ static void explain(void)
 	                "                [ besteffort | precedence | diffserv8 | diffserv4* ]\n"
 	                "                [ flowblind | srchost | dsthost | hosts | flows* ]\n"
 	                "                [ atm | noatm* ] [ overhead N | conservative | raw* ]\n"
-	                "                [ squash ]\n"
+	                "                [ squash | nosquash* ]\n"
 	                "                [ memlimit LIMIT ]\n"
 	                "    (* marks defaults)\n");
 }
